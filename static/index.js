2ee53fc442e945d7316582ef70a226da23e2eb90
Shyam Patel <56788349+shyam8898@users.noreply.github.com>
2022-09-13 13:10:56 +0530

    Add files via upload

---
+++ static/index.js
@@ -0,0 +1,21 @@
+
+
+
+
+function readUrl(input){
+	imagebox = $('#imagebox')
+	console.log("evoked readUrl")
+	if(input.files && input.files[0]){
+		let reader = new FileReader();
+		reader.onload = function(e){
+			// console.log(e)
+			
+			imagebox.attr('src',e.target.result); 
+			imagebox.height(500);
+			imagebox.width(800);
+		}
+		reader.readAsDataURL(input.files[0]);
+	}
+
+	
+}
